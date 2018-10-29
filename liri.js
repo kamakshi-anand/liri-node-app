4a3a5b5e0d24be2554a10ecc97348554139a054d
kamakshi-anand <roopa.anand@gmail.com>
2018-10-29 09:31:49 -0700

    logtxt added

--- liri.js
+++ liri.js
@@ -8,17 +8,26 @@ var keys = require('./keys.js');
 console.log("Key is " + keys.spotify);
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var logText;
 
 function spotifySearch(song) {
     spotify.search({ type: 'track', query: song }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
+        // var logText;
         for (i = 0; i < data.tracks.items.length; i++) {
-            var logText;
-            logText = "The command used is " + query + "\n" + "Album name is " + data.tracks.items[i].album.name + "\n" +
-                "Release date is " + data.tracks.items[i].album.release_date + "\n";
+
+            logText = "Album name is " + data.tracks.items[i].album.name + "\n" +
+                "Release date is " + data.tracks.items[i].album.release_date + "\n" + "Preview link is " + data.tracks.items[i].preview_url + "\n";
+            for (j = 0; j < data.tracks.items[i].album.artists.length; j++) {
+                logText = logText + "Artist name is " + data.tracks.items[i].album.artists[j].name + "\n";
+                //  console.log("Artist name is " + data.tracks.items[i].album.artists[j].name);
+                // console.log("*****************************************");
+            }
+            logText = logText + "*******************************" + "\n";
+
+
 
             fs.appendFile("log.txt", logText, function (err) {
 
@@ -53,7 +62,6 @@ function movieSearch(movie) {
     // Then run a request to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-
     // This line is just to help us debug against the actual URL.
     console.log(queryUrl);
 
@@ -63,20 +71,52 @@ function movieSearch(movie) {
         // console.log(JSON.parse(body));
         // If the request is successful (i.e. if the response status code is 200)
         console.log("Response is" + JSON.parse(body));
+
+
         if (!error && response.statusCode === 200) {
-            console.log("The movie's title is: " + JSON.parse(body).Title);
-            // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).          
-            console.log("The movie's title is: " + JSON.parse(body).Title);
-            console.log("Year the movie came out: " + JSON.parse(body).Year);
-            console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+
+            logText = "The movie's title is: " + JSON.parse(body).Title + "\n" +
+                "Year the movie came out: " + JSON.parse(body).Year + "\n" +
+                "IMDB Rating of the movie: " + JSON.parse(body).imdbRating + "\n"
             if (JSON.parse(body).Ratings[1] != null) {
-                console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+                logText = logText + "Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value + "\n";
             }
-            console.log("Country where the movie was produced: " + JSON.parse(body).Production);
-            console.log("Language of the movie: " + JSON.parse(body).Language);
-            console.log("Plot of the movie: " + JSON.parse(body).Plot);
-            console.log("Actors in the movie: " + JSON.parse(body).Actors);
+            logText = logText + "Country where the movie was produced: " + JSON.parse(body).Production + "\n" +
+                "Language of the movie: " + JSON.parse(body).Language + "\n" +
+                "Plot of the movie: " + JSON.parse(body).Plot + "\n" +
+                "Actors in the movie: " + JSON.parse(body).Actors + "\n" + "*******************" + "\n";
+
+            fs.appendFile("log.txt", logText, function (err) {
+
+                // If an error was experienced we will log it.
+                if (err) {
+                    console.log(err);
+                }
+
+                // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+                else {
+                    console.log("Content Added!");
+                }
+
+            });
+
+
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).    
+
+            //******************************************* */
+            // console.log("The movie's title is: " + JSON.parse(body).Title);
+            // console.log("Year the movie came out: " + JSON.parse(body).Year);
+            // console.log("IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+            // if (JSON.parse(body).Ratings[1] != null) {
+            //     console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+            // }
+            // console.log("Country where the movie was produced: " + JSON.parse(body).Production);
+            // console.log("Language of the movie: " + JSON.parse(body).Language);
+            // console.log("Plot of the movie: " + JSON.parse(body).Plot);
+            // console.log("Actors in the movie: " + JSON.parse(body).Actors);
+            //*************************** */
         } else {
             console.log("Could not find: " + movie);
         }
@@ -194,7 +234,7 @@ if (process.argv[2] == "my-tweets" || process.argv[2] == "do-what-it-says") {
                 }
             }
             //  console.log("Movie is" + movie);
-            twitterSearch(user,count);
+            twitterSearch(user, count);
         }
     }
     //write code
